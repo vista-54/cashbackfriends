bd19bad7b142f26088c0853af0d76e212da02349
Vista <vista-54@ukr.net>
2017-09-07 23:58:25 +0300

    Added all links

--- src/providers/url.constants.ts
+++ src/providers/url.constants.ts
@@ -1,9 +1,25 @@
-let api="https://app.cashbackfriends.online/";
+let api = "https://app.cashbackfriends.online/";
 export const URL =
     {
         login: {
-            code: api+'clients/send_auth_sms',
-            token:api+
-                'clients/get_token'
+            code: api + 'clients/send_auth_sms',
+            token: api +
+            'clients/get_token'
+        },
+        register: {
+            registration: api + 'clients/register'
+        },
+        tabs: {
+            getInfo: api + 'clients/get_info'
+        },
+        invite: {
+            getFriends: api + 'clients/get_friends'
+        },
+        card: {
+            getPurchases: api + 'clients/get_purchases',
+            getWithdrawals: api + 'clients/withdrawals'
+        },
+        catalog: {
+            getCatalog: api + 'clients/get_city_catalog'
         }
     };
