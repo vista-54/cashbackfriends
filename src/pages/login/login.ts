1fe5e37c872e2cb8b76213ea3d33b5de5dbf88ca
Vista <vista-54@ukr.net>
2017-09-07 23:13:23 +0300

    Login optimization

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,16 +1,12 @@
 import {Component} from "@angular/core";
-import {AlertController, IonicPage, LoadingController, NavController, NavParams} from "ionic-angular";
+import {IonicPage, NavController} from "ionic-angular";
 import {UserService} from "../../providers/user.service";
 import {Storage} from "@ionic/storage";
 import {MaskedInputDirective} from "angular2-text-mask";
 import {StatusBar} from "@ionic-native/status-bar";
-
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import {Message} from "../../providers/message";
+import {Connect} from "../../providers/connect";
+import {Loader} from "../../providers/loader";
 
 @Component({
     selector: 'page-login',
@@ -27,16 +23,15 @@ import {StatusBar} from "@ionic-native/status-bar";
 export class LoginPage {
 
     public user = {
-        phone: '',
+        phone: '+7',
+        code: ''
     };
-    public auth: any;
     public isCodeShow = false;
     public isLoginEnable = true;
     public isSendCodeEnable = true;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, auth: UserService, private storage: Storage,
-                public loadingCtrl: LoadingController, public alertCtrl: AlertController, public statusBar: StatusBar) {
-        this.auth = auth;
+    constructor(public navCtrl: NavController, private auth: UserService, private storage: Storage, public statusBar: StatusBar,
+                private message: Message, private connect: Connect, private loader: Loader) {
         storage.set('start_page', 'login');
         statusBar.backgroundColorByHexString('#5c45c3');
 
@@ -47,50 +42,56 @@ export class LoginPage {
         console.log('ionViewDidLoad LoginPage');
     }
 
-
+    /**
+     *
+     * @returns {boolean}
+     */
     public sendCode() {
         this.isLoginEnable = false;
         if (this.user.phone.length !== 12) {
-            let alert = this.alertCtrl.create({
-                title: 'Ошибка',
-                subTitle: 'Поле телефона должно содержать 12 цифр',
-                buttons: ['OK']
-            });
-            alert.present();
-            this.isLoginEnable = true;
+            this.message.show('Ошибка', 'Поле телефона должно содержать 12 цифр');
+            return false;
+        }
+        if (!this.connect.isConnected) {
+            this.message.show('Ошибка сети', 'Подключение отсуствует');
             return false;
         }
         this.auth.code(this.user)
             .subscribe(
                 data => {
-                    if (data.status === 200) {
-                        this.isCodeShow = true;
-                    }
+                    this.isCodeShow = true;
                 },
                 err => {
-                    this.isLoginEnable = true;
-                    let errorText = JSON.parse(err._body);
-                    let alert = this.alertCtrl.create({
-                        title: 'Ошибка',
-                        subTitle: errorText.phone[0],
-                        buttons: ['OK']
-                    });
-                    alert.present();
+                    this.loader.hide();
+                    let errorText: any;
+                    try {
+                        errorText = JSON.parse(err._body);
+                        this.message.show('Ошибка', errorText.code || errorText.errors)
+                    }
+                    catch (e) {
+                        this.message.show('Ошибка', 'Неизвестная ошибка')
+                    }
                 }
             )
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     public login() {
-        let loader = this.loadingCtrl.create({
-            content: "Пожалуйста, подождите...",
-        });
-        loader.present();
-
+        if (this.user.code.length !== 6) {
+            this.message.show('Ошибка', 'Проверочный код введен не корректно');
+            return false;
+        }
+        if (!this.connect.isConnected) {
+            this.message.show('Ошибка сети', 'Подключение отсуствует');
+            return false;
+        }
         this.auth.getToken(this.user)
             .subscribe(
                 data => {
                     if (data.token) {
-                        loader.dismiss();
                         this.storage.set('token', data.token);
                         if (data.is_registered) {
                             this.navCtrl.push('tabs');
@@ -100,15 +101,15 @@ export class LoginPage {
                     }
                 },
                 err => {
-                    let errorText = JSON.parse(err._body);
-                    loader.dismiss();
-                    let alert = this.alertCtrl.create({
-                        title: 'Ошибка',
-                        subTitle: errorText.code || errorText.errors,
-                        buttons: ['OK']
-                    });
-                    alert.present();
-
+                    this.loader.hide();
+                    let errorText: any;
+                    try {
+                        errorText = JSON.parse(err._body);
+                        this.message.show('Ошибка', errorText.code || errorText.errors)
+                    }
+                    catch (e) {
+                        this.message.show('Ошибка', 'Неизвестная ошибка')
+                    }
                 },
             )
 
